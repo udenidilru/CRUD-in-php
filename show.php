6ae4ba38ab0a149d529a29417d4eb6e425b27791
Udeni dilrukshi <40499515+udenidilru@users.noreply.github.com>
2019-09-07 22:29:00 +0530

    I created the show.php file

---
+++ show.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style.css">
+<div class="h1">products</div>
+
+<div class="ul">
+	<div class="li"><a href="insert.php">Insert data</a></div>
+	<div class="li"><a href="update.php">Update data</a></div>
+	<div class="li"><a href="delete.php">Delete data</a></div>
+	<div class="li"><a href="display.php">Display data</a></div>
+</div>
